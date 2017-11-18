d616e490ca347b24462a67d0dd64039630183d66
24032 <24032@DESKTOP-OVBKG5K>
2017-11-18 16:32:19 +0800

    16:2

--- MultiTalking/src/group/li/JTreeRender.java
+++ MultiTalking/src/group/li/JTreeRender.java
@@ -3,6 +3,11 @@ package group.li;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
 import javax.swing.JTree;
@@ -10,15 +15,29 @@ import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class JTreeRender extends DefaultTreeCellRenderer{
+	//添加字库方法
+	public Font getFont(float size) {  
+        String pathString = getClass().getClassLoader().getResource("HanYix.ttf").getFile();  
+        try {  
+            Font dynamicFont = Font.createFont(Font.PLAIN, new File(pathString));  
+            dynamicFont = dynamicFont.deriveFont(size);  
+            return dynamicFont;  
+        } catch (FontFormatException ex) {  
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);  
+        } catch (IOException ex) {  
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);  
+        }  
+          
+        return null;  
+} 
 	
 	public Component getTreeCellRendererComponent(JTree tree, Object value,  
             boolean sel, boolean expanded, boolean leaf, int row,  
             boolean hasFocus) {  
 		
-		this.setFont(new Font("宋体",Font.PLAIN, 20));
+		this.setFont(this.getFont(20.0f));
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;  
-        // 鏍硅妭鐐逛粠0寮�濮嬶紝渚濇寰�涓�  
-        // 鍒嗙粍  
+
         if (node.getLevel() == 1) {  
             if (expanded) {  
                 this.setIcon(new ImageIcon(getClass().getClassLoader().getResource("icon/151.png")));  
